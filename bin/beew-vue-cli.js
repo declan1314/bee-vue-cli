b7e71b0644745038ebeda7a57467766cc4c6f585
吴德源 <0268000105@zte.com.cn>
2019-11-19 15:08:59 +0800

    1. 优化template目录结构，以便下一步init操作。 2. 对文件合适命名

--- bin/beew-vue-cli.js
+++ bin/beew-vue-cli.js
@@ -29,18 +29,6 @@ function cleanArgs (cmd) {
 program
   .command('create <module-name>')
   .description('create a new module powered by beew-vue-cli')
-  .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset')
-  .option('-d, --default', 'Skip prompts and use default preset')
-  .option('-i, --inlinePreset <json>', 'Skip prompts and use inline JSON string as preset')
-  .option('-m, --packageManager <command>', 'Use specified npm client when installing dependencies')
-  .option('-r, --registry <url>', 'Use specified npm registry when installing dependencies (only for npm)')
-  .option('-g, --git [message]', 'Force git initialization with initial commit message')
-  .option('-n, --no-git', 'Skip git initialization')
-  .option('-f, --force', 'Overwrite target directory if it exists')
-  .option('-c, --clone', 'Use git clone when fetching remote preset')
-  .option('-x, --proxy', 'Use specified proxy when creating project')
-  .option('-b, --bare', 'Scaffold project without beginner instructions')
-  .option('--skipGetStarted', 'Skip displaying "Get started" instructions')
   .action(async (name, cmd) => {
     const options = cleanArgs(cmd)
 
@@ -51,6 +39,14 @@ program
     if (process.argv.includes('-g') || process.argv.includes('--git')) {
       options.forceGit = true
     }
-    await require('../lib/index.js')(name)
+    await require('../lib/creator.js')(name)
   })
+
 program.parse(process.argv);
+
+// add some useful info on help
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan(`beew-vue-cli <command> --help`)} for detailed usage of given command.`)
+  console.log()
+})
